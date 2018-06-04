fbcc0f6cee6823732679ea87b8af7d2e9927a61e
Marvin Kang'ethe <marvin.kangethe@andela.com>
2018-06-04 04:53:30 +0300

    Setup the seed data

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,3 @@
 class Planet < ApplicationRecord
+  has_one :chemical_composition
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+planets = [
+  {
+    name: "Mercury",
+    size: 2440,
+    compositions: "iron",
+    rotation_time: "58 days"
+  },
+  {
+    name: "venus",
+    size: 6052,
+    compositions: "liquid rock",
+    rotation_time: "253 days"
+  },
+  {
+    name: "earth",
+    size: 6371,
+    compositions: "solid rock",
+    rotation_time: "1 day"
+  },
+  {
+    name: "mars",
+    size: 3390,
+    compositions: "oxidised iron dust",
+    rotation_time: "1 day"
+  },
+  {
+    name: "jupiter",
+    size: 69911,
+    compositions: "hydrogen and helium",
+    rotation_time: "0.5 days"
+  }
+]
+
+compositions = ["hydrogen", "helium", "solid rock", "liquid rock", "iron"]
+
+ChemicalComposition.destroy_all
+Planet.destroy_all
+
+planets.each do |p|
+  this_planet = Planet.new(
+                  name: p[:name],
+                  size: p[:size],
+                  rotation_time: p[:rotation_time]
+                )
+  this_planet.save
+end
+
+planet_ids = Planet.all.pluck(:id)
+
+compositions.each do |comp|
+  ChemicalComposition.create(
+    name: comp,
+    planet_id: planet_ids.sample)
+end
+
+puts "All data created"
